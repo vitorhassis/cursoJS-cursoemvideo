4d476a654f136390537d96637e693c6dd09f0d0b
Vitor Assis <vitorassis997@gmail.com>
2024-04-08 23:46:17 -0300

    att

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1;
+do {
+  console.log(`passo ${c}`);
+  c++;
+} while (c <= 6); //ou seja, *FAÇA* com que APAREÇA `PASSO ${}`, SENDO O VALOR DO CONTADOR A SOMA DELE COM MAIS UM (c++) ENQUANTO o valor do contador for menor ou igual a seis
+
+/*var c = 1;
+while (c <= 10) {
+  console.log(`passo ${c}`);
+  c++; //c = c+1, contador mais um  //ou seja, *ENQUANTO* O VALOR DO CONTADOR FOR MENOR OU IGUAL A 10, DIGITE NO CONSOLE `PASSO `${}`, somando o contador +1 
+}
+*/
+
+/*
+console.log ('tudo bem?')
+console.log("tudo bem?");
+console.log("tudo bem?");
+console.log("tudo bem?");  ambos codigos tem a mesma ideia, porem a de cima claramente eh mais eficiente e facil de ser usada
+console.log("tudo bem?");
+console.log("tudo bem?");
+console.log ('tudo bem?')
+*/
